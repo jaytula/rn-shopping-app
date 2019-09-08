d6206568225a2b00c994ca8bd76727b9a0c3855a
Jay T <jaytula@gmail.com>
2019-09-08 11:08:24 -0700

    Lesson 158: The Products Overview Screen

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const ProductsOverviewScreen = props => {
+  return (
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {}
+});
+
+export default ProductsOverviewScreen;
