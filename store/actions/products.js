e36a4a642c47bdb46b973353202cb3bf04142291
Jay T <jaytula@gmail.com>
2019-09-14 09:33:41 -0700

    Lesson 196: Storing Products on a Server

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,3 +1,5 @@
+import { FIREBASE_DB } from "../../config";
+
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
@@ -8,14 +10,27 @@ export const deleteProduct = id => ({
 });
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price
-    }
+  return async dispatch => {
+    const response = await fetch(FIREBASE_DB, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ title, description, imageUrl, price })
+    });
+
+    const resData = await response.json();
+    console.log({ resData });
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.name,
+        title,
+        description,
+        imageUrl,
+        price
+      }
+    });
   };
 };
 
