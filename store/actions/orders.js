527788b2380a4c79db57304afc0c33449824718c
Jay T <jaytula@gmail.com>
2019-09-20 23:25:29 -0700

    Lesson 202: Storing Orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,8 +1,29 @@
+import { FIREBASE_DB } from "../../config";
 export const ADD_ORDER = "PLACE_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount }
+ 
+  return async dispatch => {
+    const date = new Date();
+    const response = await fetch(`${FIREBASE_DB}/orders/u1.json`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        cartItems,
+        totalAmount,
+        date: date.toISOString()
+      })
+    });
+
+    resData = await response.json();
+
+    if(!response.ok) throw new Error('Something went wrong');
+    console.log(response);
+    dispatch({
+      type: ADD_ORDER,
+      orderData: { id: resData.name, items: cartItems, amount: totalAmount, date }
+    });
   };
 };
